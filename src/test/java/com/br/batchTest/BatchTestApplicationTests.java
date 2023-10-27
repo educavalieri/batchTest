6ec9f1cbe27c0818b384c6f96d55d3322c2c92b9
educavalieri <D@rkman1969>
2023-10-26 21:28:39 -0300

    Taskless Step exemple

---
+++ src/test/java/com/br/batchTest/BatchTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.batchTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
